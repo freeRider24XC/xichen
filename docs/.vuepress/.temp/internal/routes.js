0e9ac46d96d7093a3e6f101aee72656e3918fe90
tankangping <tankangping@mooncell.com.cn>
2024-08-08 16:47:56 +0800

    build(架构): 二次构建

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -7,20 +7,3 @@ export const routes = Object.fromEntries([
   ["/", { loader: () => import(/* webpackChunkName: "index.html" */"E:/xichen/xichen_knight/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"西辰knight"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"E:/xichen/xichen_knight/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateRoutes) {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-  }
-  if (__VUE_HMR_RUNTIME__.updateRedirects) {
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ routes, redirects }) => {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  })
-}
