75b6c18bc5d7e7ffe77b50077cf8ac347d687650
tankangping <tankangping@mooncell.com.cn>
2024-08-12 17:32:20 +0800

    style: 规避单词拼写问题

--- docs/.vuepress/.temp/pages/blog.html.vue
+++ docs/.vuepress/.temp/pages/blog.html.vue
@@ -1,8 +1,3 @@
-<template><div><h1 id="博客" tabindex="-1"><a class="header-anchor" href="#博客"><span>博客</span></a></h1>
-<h2 id="flutter" tabindex="-1"><a class="header-anchor" href="#flutter"><span>flutter</span></a></h2>
-<h2 id="vue" tabindex="-1"><a class="header-anchor" href="#vue"><span>vue</span></a></h2>
-<h2 id="react" tabindex="-1"><a class="header-anchor" href="#react"><span>react</span></a></h2>
-<h2 id="kotlin" tabindex="-1"><a class="header-anchor" href="#kotlin"><span>kotlin</span></a></h2>
-</div></template>
+<template><div></div></template>
 
 
--- docs/.vuepress/.temp/pages/photograph.html.vue
+++ docs/.vuepress/.temp/pages/photograph.html.vue
@@ -1,9 +1,3 @@
-<template><div><h1 id="摄影" tabindex="-1"><a class="header-anchor" href="#摄影"><span>摄影</span></a></h1>
-<h2 id="人像" tabindex="-1"><a class="header-anchor" href="#人像"><span>人像</span></a></h2>
-<h2 id="风光" tabindex="-1"><a class="header-anchor" href="#风光"><span>风光</span></a></h2>
-<h2 id="人文" tabindex="-1"><a class="header-anchor" href="#人文"><span>人文</span></a></h2>
-<h2 id="建筑" tabindex="-1"><a class="header-anchor" href="#建筑"><span>建筑</span></a></h2>
-<h2 id="器材" tabindex="-1"><a class="header-anchor" href="#器材"><span>器材</span></a></h2>
-</div></template>
+<template><div></div></template>
 
 
